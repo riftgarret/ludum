fa37295e4390dddecd9d9453802c57e06bc8008d
Garret Cole <riftgarret@users.noreply.github.com>
2018-09-18 21:40:19 -0700

    update git ignore import hello world into project space.

---
+++ App/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Build
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        [SerializeField]
+        private float rate = 0.5f;
+
+        [SerializeField]
+        private float radius = 5f;
+
+        public void Update()
+        {
+            //gameObject.transform.position.x = radius * Math.Sin(rate * Time.fixedTime);
+        }
+    }
+}
