1de0b6cea263a088e7aaf949d793a8812b49ba07
Garret Cole <riftgarret@users.noreply.github.com>
2018-10-04 23:29:42 -0700

    Implementation for decision making and targeting

---
+++ Redninja/BattleSystem/Targeting/SelectedTarget.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redninja.BattleSystem.Targeting
+{
+    /// <summary>
+    /// Selected Target should represent the required meta data for the TargetType.
+    /// </summary>
+    public class SelectedTarget
+    {
+        public bool IsPosition => TargetEntity == null;
+
+        public IBattleEntity TargetEntity { get; }        
+
+        public int AnchoredPositionRow { get; }
+
+        public int AnchoredPositionColumn { get; }
+
+        public int TeamSide { get; } // TODO rice comment 
+
+        private SelectedTarget(         
+            IBattleEntity targetEntity,             
+            int anchoredPositionRow, 
+            int anchoredPositionColumn, 
+            int teamSide)
+        {            
+            TargetEntity = targetEntity;            
+            AnchoredPositionRow = anchoredPositionRow;
+            AnchoredPositionColumn = anchoredPositionColumn;
+            TeamSide = teamSide;
+        }
+
+        public static SelectedTarget CreateEntityTarget(IBattleEntity target)
+            => new SelectedTarget(target, -1, -1, -1);
+
+        public static SelectedTarget CreatePositionTarget(int row, int col, int teamSide)
+            => new SelectedTarget(null, row, col, teamSide);
+    }
+}
---
+++ Redninja/BattleSystem/Targeting/TargetType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redninja.BattleSystem.Targeting
+{
+    /// <summary>
+    /// Target type represents the type of targetting that can happen.
+    /// </summary>
+    public enum TargetType
+    {
+        Target, // targets are not based on tiles
+        Pattern, // targets are based on pattern on tiles
+        TargetAndPattern // target is center of pattern
+    }
+}
