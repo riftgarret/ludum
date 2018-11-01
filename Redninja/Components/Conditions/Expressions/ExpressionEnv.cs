dcc480e41d05fd4950726b4194b710eeba556f64
Garret Cole <gcole@blackpixel.com>
2018-11-01 07:30:20 -0700

    add ExpressionEnv

---
+++ Redninja/Components/Conditions/Expressions/ExpressionEnv.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Davfalcon.Builders;
+using Redninja.Events;
+
+namespace Redninja.Components.Conditions.Expressions
+{
+	internal class ExpressionEnv : IExpressionEnv
+	{
+		private IUnitModel self;
+		private IUnitModel target;
+		private IUnitModel source;
+		private IBattleModel battleModel;
+		private IBattleEvent battleEvent;
+
+		public IUnitModel Self => ReturnOrThrow(self, "SELF");
+		public IUnitModel Source => ReturnOrThrow(source, "SOURCE");
+		public IUnitModel Target => ReturnOrThrow(self, "TARGET");
+		public IBattleModel BattleModel => ReturnOrThrow(battleModel, "BATTLE");
+		public IBattleEvent BattleEvent => ReturnOrThrow(battleEvent, "EVENT");
+
+		private ExpressionEnv()
+		{
+		}
+
+		private T ReturnOrThrow<T>(T item, string propName) where T : class
+		{
+			if (item == null) throw new InvalidOperationException($"Missing environment property: {propName}");
+			return item;
+		}
+
+		public static ExpressionEnv From(IBattleModel model, IUnitModel self, IUnitModel target)
+		{
+			ExpressionEnv env = new ExpressionEnv();
+			env.self = self;
+			env.battleModel = model;
+			env.target = target;
+			return env;
+		}
+
+		public static ExpressionEnv From(IBattleModel model, IUnitModel self, IBattleEvent battleEvent)
+		{
+			ExpressionEnv env = new ExpressionEnv();
+			env.source = battleEvent.Entity;
+			env.self = self;
+			env.battleModel = model;
+			if(battleEvent is ITargetedEvent) 
+				env.target = ((ITargetedEvent)battleEvent).Target;
+			env.battleEvent = battleEvent;
+			return env;
+		}
+	}
+}
---
+++ Redninja/Components/Conditions/Expressions/IExpressionEnv.cs
@@ -0,0 +1,13 @@
+﻿using Redninja.Events;
+
+namespace Redninja.Components.Conditions.Expressions
+{
+    internal interface IExpressionEnv
+    {
+        IUnitModel Self { get; }
+        IUnitModel Target { get; }
+		IUnitModel Source { get; }
+		IBattleModel BattleModel { get; }
+        IBattleEvent BattleEvent { get; }
+    }
+}
