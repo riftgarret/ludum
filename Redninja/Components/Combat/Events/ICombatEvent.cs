0bcdd0f56b51582e1d1cbd4758b3b82ed9e3505b
Garret Cole <riftgarret@users.noreply.github.com>
2018-11-06 18:20:36 -0800

    combat event interface adjustments

--- Redninja/Components/Combat/Events/ICombatEvent.cs
+++ Redninja/Components/Combat/Events/ICombatEvent.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Redninja.Components.Combat.Events
 {
 	/// <summary>
@@ -7,6 +10,56 @@ namespace Redninja.Components.Combat.Events
 	{
 		IUnitModel Source { get; }
 		IUnitModel Target { get; }
-	} 
+		
+		IEnumerable<CombatEventFlag> CombatFlags { get; }
+
+		T Get<T>(CombatEventAttribute attribute);
+	}
+
+	/// <summary>
+	/// Helper methods
+	/// </summary>
+	public static class ICombatEventExt
+	{
+		public static bool HasFlag(this ICombatEvent combatEvent, CombatEventFlag flag)
+			=> combatEvent.CombatFlags.Contains(flag);
+	}
+
+	public enum CombatEventType
+	{
+		Healing,
+		Damage,
+		Movement,
+		StatusEffect
+	}
+
+	public enum CombatEventFlag
+	{
+		Death,
+		Resurection,		
+
+		StatusEffectRemoved,
+		StatusEffectAdded,
+	}
+
+	public enum CombatEventAttribute
+	{
+		// int values
+		TotalAmount,
+		Amount,
+		CriticalAmount,
+
+		// UnitPosition
+		OriginalPosition,
+		NewPosition,
+
+		// IStatusEffect
+		StatusEffect,
+
+		// ISkill
+		Skill
+	}
+
+	
 }
 
